4e17a51de39811bc9a08c9b12763b53a68cb44fe
Ramon dos Santos <ramontedescodossantos2020@gmail.com>
2021-09-22 15:27:11 -0300

    Alteracao no arquivo README.md

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Console;
+
+namespace Tabuada_For
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int rec;
+
+            rec = 1;
+            while(rec > 0 & rec < 2)      
+            {    
+                WriteLine(""); 
+
+                WriteLine("Tabuada do Número: ");
+                n = Convert.ToInt32(ReadLine());
+
+                for(int i = 1 ; i<11 ; i++){
+                    WriteLine($"{i} x {n} = {i * n}");
+                };
+
+                WriteLine(""); 
+
+                WriteLine("Para recalcular, digite 1 e pressione ENTER. Para fechar, digite 0 e pressione ENTER");
+                rec = Convert.ToInt32(ReadLine()); 
+
+                WriteLine(""); 
+                WriteLine("------------------");
+                WriteLine(""); 
+            }
+
+            WriteLine(" Obrigado por utilizar o programa !! "); 
+
+            WriteLine(""); 
+        }
+    }
+}
